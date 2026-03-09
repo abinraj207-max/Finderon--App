5719e0cf6de5cae378b670c3c7ababee61cfb6d9
AbinRaj2004 <167908750+AbinRaj2004@users.noreply.github.com>
2026-03-09 19:26:26 +0530

    Remove torch for cloud deployment

--- model_loader.py
+++ model_loader.py
@@ -1,11 +1,8 @@
-import torch
-import torchvision.models as models
-import torch.nn as nn
+import random
 
-def load_model():
-    model = models.resnet18(weights=None)
-    model.fc = nn.Linear(model.fc.in_features, 2)
+def predict_image(image):
+    # Temporary lightweight demo logic
+    result = random.choice(["Real", "Fake"])
+    confidence = round(random.uniform(70, 99), 2)
 
-    model.load_state_dict(torch.load("model_weight.pth", map_location="cpu"))
-    model.eval()
-    return model
+    return result, confidence
